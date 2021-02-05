ab02ff9c3ea1bdc8df8099eabcc01bed57ac50fd
Gene Howell <gene.howell9@gmail.com>
2021-02-04 18:37:59 -0600

    Added and imported Nunito fonts to fonts folder and imported AppLoading from Expo

--- App.js
+++ App.js
@@ -1,8 +1,43 @@
-import React from 'react';
-import Home from './screens/home'
+import React, {useState} from 'react';
+import Home from './screens/home';
+import { StyleSheet, Text, View } from 'react-native';
+import * as Font from 'expo-font'
+import AppLoading from 'expo-app-loading';
+
+const getFonts = () => Font.loadAsync({
+    'Nunito-Regular':require('./assets/fonts/Nunito-Regular.ttf'),
+    'Nunito-Bold': require('./assets/fonts/Nunito-Bold.ttf')
+  });
+
 
 export default function App() {
-  return (
-    <Home />
-  );
+
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
+
+  if(fontsLoaded){
+    return (
+      <Home />
+    );
+  } else {
+    return (
+    <AppLoading
+     startAsync={getFonts}
+     onFinish={()=> setFontsLoaded(true)} 
+     onError={() => console.log('error')}
+    />
+    )
+  }
+
 }
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
